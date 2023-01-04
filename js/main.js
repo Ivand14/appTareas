2aba527cd59acca2ad1a8db9ecaf011188ba1eda
Ivan <carivan14@homtail.com>
2023-01-04 15:13:18 -0300

    listo

--- js/main.js
+++ js/main.js
@@ -1,23 +1,70 @@
-let nombre= prompt('Ingresa tu nombre')
+function nombreValido() {
+  let ingresarNombre = prompt("Bienvenido a todo list app. Cual es tu nombre?");
 
-while(nombre==''){
-    alert('Ingresa un nombre valido')
-    nombre=prompt('Vuelve a ingresar el nombre')
+  while (ingresarNombre != "") {
+    alert(`${ingresarNombre} Bienvenido a todo list app`);
+
+    break;
+  }
+  while (ingresarNombre == "") {
+    alert("Ingresa un nombre correcto");
+    ingresarNombre = prompt("vuelve a ingresar tu nombre");
+  }
 }
 
-do {
-    alert(nombre+' Bienvenido a todo list app ')
-    nombre=prompt('¿ Desea Continuar? Presione enter')
-} while (nombre!='');
-
-let alarma=prompt('¿Desea configurar una alarma? Si/No ')
-
-while(alarma=='Si'){
-    alarma=prompt('¿A que hora desea su alarma?')
-    alert('Su alarma fue programada a las '+ alarma)
+function fechas(cumple) {
+  console.log(`se agendo la fecha de su cumpleaños ${cumple}`);
 }
 
+function datos() {
+  let mail = prompt("Ingrese su correo electronico");
+  while (mail == "") {
+    mail = prompt("Vuelva a ingresar su correo electronico");
+  }
+  let contraseña = prompt("Ingrese su contraseña");
 
+  for (let i = 0; i < 1; i++) {
+    contraseña = prompt("Confirme su contraseña");
+  }
+}
 
+function recordatorio() {
+  let recordar = prompt("¿Desea programar una alarma? Si/No");
 
+  switch (recordar) {
+    case "Si":
+      recordar = prompt("¿A que hora desea su alarma?");
+      alert(`su alarma fue programada a las ${recordar}`);
+      break;
+    case "No":
+      alert("No se programará ninguna a alarma");
+      break;
+    default:
+      recordatorio();
+  }
+}
 
+function notificacion() {
+  let notificaciones = prompt("¿Desea recibir notificaciones? Si/No");
+
+  switch (notificaciones) {
+    case "Si":
+      alert(`Usted recibira notificaciones`);
+      break;
+    case "No":
+      alert("No recibira notificaciones");
+      break;
+    default:
+      notificacion();
+  }
+}
+
+function fechas(cumple) {
+  console.log(`se agendo la fecha de su cumpleaños ${cumple}`);
+}
+
+nombreValido();
+let fecha = fechas("04/11/1999");
+datos();
+recordatorio();
+notificacion();
